d23bf1e4d7838f9825360f965427403778fb886e
giselledunine <55180310+giselledunine@users.noreply.github.com>
2021-11-03 15:11:23 +0100

    Decimal to Roman numbers 0 to 10

--- src/BankAccount.php
+++ src/BankAccount.php
@@ -11,12 +11,32 @@ protected string $roman;
 
     public function getRoman(int $number): string
     {
-
         if($number === 0){
             return $this->roman;
         }
         if($number <=3 && $number > 0) {
             $this->roman = str_repeat('I', $number);
+            return $this->roman;
+        }
+        if($number === 4){
+            $this->roman = "IV";
+            return $this->roman;
+        }
+        if($number === 5){
+            $this->roman = "V";
+            return $this->roman;
+        }
+        if($number >= 6 && $number <= 8 ) {
+            $this->roman = "V".str_repeat('I', $number-5);
+            return $this->roman;
+        }
+
+        if($number === 9){
+            $this->roman = "IX";
+            return $this->roman;
+        }
+        if($number === 10){
+            $this->roman = "IX";
             return $this->roman;
         }
     }
--- tests/BankAccountTest.php
+++ tests/BankAccountTest.php
@@ -11,5 +11,12 @@ final class BankAccountTest extends TestCase {
         $this->assertEquals("I", $roman->getRoman(1));
         $this->assertEquals("II", $roman->getRoman(2));
         $this->assertEquals("III", $roman->getRoman(3));
+        $this->assertEquals("IV", $roman->getRoman(4));
+        $this->assertEquals("V", $roman->getRoman(5));
+        $this->assertEquals("VI", $roman->getRoman(6));
+        $this->assertEquals("VII", $roman->getRoman(7));
+        $this->assertEquals("VIII", $roman->getRoman(8));
+        $this->assertEquals("IX", $roman->getRoman(9));
+        $this->assertEquals("X", $roman->getRoman(10));
     }
 }
